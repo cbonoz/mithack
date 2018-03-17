901e13b5fdee86c43bae0421f9d9d4d51cb75a4a
Brandon In <brandonin@me.com>
2018-03-17 12:10:20 -0400

    added a missing parameter

--- server/neon.js
+++ server/neon.js
@@ -39,7 +39,7 @@ const library = (function() {
             scriptHash: "23ba2703c53263e8d6e522dc32203339dcd8eee9",
             operation: 'retrieve',
             // args: sc.ContractParam.array(param1, Neon.u.reverseHex('cef0c0fdcfe7838eff6ff104f9cdec2922297537'))
-            args: sc.ContractParam.array(address)
+            args: sc.ContractParam.array(address + "_" + hash);
         });
 
         config.script = script;
